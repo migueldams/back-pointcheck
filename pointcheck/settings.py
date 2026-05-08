094c7e2d85b6128196e2816dc515bed522b14349
migueldams <douanlmiguel@gmail.com>
2026-05-08 15:40:44 +0100

    first commit

--- pointcheck/settings.py
+++ pointcheck/settings.py
@@ -115,6 +115,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
+    "https://front-pointcheck.vercel.app/",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
