f02beddf298dca12326603437e06829c282fca59
migueldams <douanlmiguel@gmail.com>
2026-05-09 01:43:56 +0100

    first commit

--- pointcheck/settings.py
+++ pointcheck/settings.py
@@ -16,18 +16,19 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     # Third party
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
     'django_filters',
+
     # Local apps
     'apps.accounts',
     'apps.attendance',
