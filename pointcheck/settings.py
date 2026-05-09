19a10536fc9ae64b178e4bcf1112a032285e3018
migueldams <douanlmiguel@gmail.com>
2026-05-09 01:14:37 +0100

    first commit

--- pointcheck/settings.py
+++ pointcheck/settings.py
@@ -3,7 +3,10 @@ Configuration Django pour PointCheck
 """
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 import os
+import psycopg2
+from dotenv import load_dotenv
 from corsheaders.defaults import default_headers
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -62,11 +65,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pointcheck.wsgi.application'
 
+load_dotenv()
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTH_USER_MODEL = 'accounts.User'
